2c32579a88d2343d2b4774c899af229655bba57c
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-02-17 22:15:09 +0330

    add todo

--- redux-2/src/App.js
+++ redux-2/src/App.js
@@ -1,56 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-
+import React from "react";
+import { useState } from "react";
+import "rsuite/dist/styles/rsuite-dark.css";
+import { useDispatch } from "react-redux";
+// import { Counter } from './features/counter/Counter';
+import Todos from "./features/todos/Todos";
+import Posts from "./features/posts/Posts";
+import Users from "./features/users/Users";
+import {
+  Button,
+  Container,
+  Header,
+  Navbar,
+  Content,
+  Panel,
+  Form,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  ButtonToolbar,
+  Divider,
+  FlexboxGrid,
+} from "rsuite";
+import "./App.css";
+import FlexboxGridItem from "rsuite/esm/FlexboxGrid/FlexboxGridItem";
 function App() {
+  const [todoTxt, setTodoTxt] = useState("");
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="main">
+      <Container>
+        <Header>
+          <Navbar appearance="inverse">
+            <Navbar.Header>
+              <p className="navbar-brand">Redux React TODO</p>
+            </Navbar.Header>
+          </Navbar>
+        </Header>
+        <Content>
+          <FlexboxGrid justify="center">
+            <FlexboxGridItem colspan={12}>
+              <Panel header={<h3>Add ToDo</h3>} bordered>
+                <Form fluid>
+                  <FormGroup>
+                    <ControlLabel>What you want to do ? </ControlLabel>
+                    <FormControl
+                      name="task"
+                      value={todoTxt}
+                      onChange={setTodoTxt}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <ButtonToolbar>
+                      <Button
+                        appearance="primary"
+                        onClick={() => {
+                          dispatch(
+                            add({ txt: todoTxt, id: Date.now().toString() })
+                          );
+                          setTodoTxt("");
+                        }}
+                      >
+                        Create
+                      </Button>
+                    </ButtonToolbar>
+                  </FormGroup>
+                </Form>
+              </Panel>
+              <Divider />
+              <Todos />
+              <Posts />
+              <Users />
+            </FlexboxGridItem>
+          </FlexboxGrid>
+        </Content>
+      </Container>
     </div>
   );
 }
