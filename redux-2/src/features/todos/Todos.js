5d1f63c98eef89b6c2bb08134c4f674249e743f6
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-02-21 23:06:08 +0330

    del fun

--- redux-2/src/features/todos/Todos.js
+++ redux-2/src/features/todos/Todos.js
@@ -1,17 +1,33 @@
 import { useSelector, useDispatch } from "react-redux";
-import { selectTodos, del } from "./todosSlice";
-export default function Todos() {
+import { selectTodos, del, patch, delProp } from "./todosSlice";
+export default function Todos(props) {
   const todos = useSelector(selectTodos);
   const dispatch = useDispatch();
   return (
     <ol>
-      {todos.map(({ txt, id }) => (
+      {todos.map(({ txt, id }, index) => (
         <li key={id}>
           {txt}
           {/* for delete */}
           <i
+            className="fa-solid fa-circle-minus"
+            onClick={() => {
+              dispatch(delProp({ index, prop: "txt" }));
+              // <input type="text" />;
+            }}
+          ></i>
+          <i
+            className="fa-solid fa-pencil"
+            onClick={() => {
+              dispatch(patch({ index, txt: props }));
+              <input type="text" />;
+            }}
+          ></i>
+          <i
             className="fa-solid fa-trash-can"
-            onClick={() => dispatch(del(id))}
+            // slove differnet
+            onClick={() => dispatch(del(index))}
+            // onClick={() => dispatch(del(id))}
           ></i>
         </li>
       ))}
