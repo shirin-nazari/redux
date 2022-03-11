828eb05a9ef4b7f716bbfc119625ccc375743767
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-03-11 18:58:59 +0330

    add styles

--- muininja/src/pages/Create.js
+++ muininja/src/pages/Create.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 // import AcUnitIcon from "@mui/icons-material/AcUnit";
@@ -8,26 +8,33 @@ import Container from "@mui/material/Container";
 import { makeStyles } from "@mui/styles";
 import TextField from "@mui/material/TextField";
 const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-
-    // background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    "&:hover": {
-      background: "red",
-      fontSize: 80,
-    },
-  },
   title: {
     marginBottom: 20,
   },
   field: {
-    marginTop: 20,
-    marginBottom: 20,
+    marginTop: 50,
+    marginBottom: 100,
     display: "block",
   },
 });
 export default function Create() {
   const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+    if (title === "") setTitleError(true);
+    if (details === "") setDetailsError(true);
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
   return (
     <Container>
       <Typography
@@ -39,15 +46,18 @@ export default function Create() {
       >
         Create a New Notes
       </Typography>
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
         <TextField
+          onChange={(e) => setTitle(e.target.value)}
           className={classes.field}
           label="Note Title"
           color="secondary"
           fullWidth
           required
+          error={titleError}
         />
         <TextField
+          onChange={(e) => setDetails(e.target.value)}
           className={classes.field}
           label="Details"
           color="secondary"
@@ -55,19 +65,20 @@ export default function Create() {
           rows={4}
           fullWidth
           required
+          error={detailsError}
         />
+        <Button
+          className={classes.btn}
+          type="submit"
+          color="secondary"
+          variant="contained"
+          startIcon={<SendIcon />}
+          endIcon={<KeyboardArrowRightIcon />}
+          disableElevation
+        >
+          submit
+        </Button>{" "}
       </form>
-      <Button
-        className={classes.btn}
-        type="submit"
-        color="secondary"
-        variant="contained"
-        startIcon={<SendIcon />}
-        endIcon={<KeyboardArrowRightIcon />}
-        disableElevation
-      >
-        submit
-      </Button>{" "}
       {/* <AcUnitIcon color="secondary" fontSize="small" />
       <AcUnitIcon color="action" fontSize="large" />
       <AcUnitIcon color="error" fontSize="small" /> */}
